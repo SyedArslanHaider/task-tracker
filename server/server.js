4cb92953c263b55a5b7baea9ecdac15751a188f9
Syed Arslan <arslanhaider231@gmail.com>
2025-03-08 14:27:45 +0100

    add a task.json file and get task data as json formate

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const express = require('express');
+import express from "express";
+import cors from "cors";
 const app = express();
 import {allTasks} from './data/tasks.js';
 const port = process.env.PORT || 3333;
 
 // ...existing code...
 app.use(express.json());
-
+app.use(cors());
 app.get('/tasks', (req, res) => {
-	res.json(allTasks);
+	res.json(Array.from(allTasks.values()));
 });
 
 app.post('/tasks', (req, res) => {
